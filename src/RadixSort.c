66a8937eb45ea98bc6c9492f1d034010038d8e0e
yenng <william951721@gmail.com>
2015-10-10 19:02:38 +0800

    Added function of finding largest number and its significant number.

--- src/RadixSort.c
+++ src/RadixSort.c
@@ -1,1 +1,26 @@
 #include "RadixSort.h"
+#include <limits.h>
+
+int getLargestNum(int *array){
+	int largestNum = INT_MIN;
+	int i;
+	for(i = 0; i < sizeof(array); i++){
+		if(array[i] > largestNum){
+			largestNum = array[i];
+		}
+	}
+	return largestNum;
+}
+
+int getSignificantNum(int largestNum){
+	int sigNum = 0;
+	while(largestNum >= 10){
+		largestNum = largestNum / 10;
+		sigNum++;
+	}
+	return sigNum;
+}
+
+int RadixSort(int array){
+	
+}
--- test/test_RadixSort.c
+++ test/test_RadixSort.c
@@ -9,7 +9,17 @@ void tearDown(void)
 {
 }
 
-void test_module_generator_needs_to_be_implemented(void)
+void test_find_significant_number_of_largest_number(void)
 {
-	TEST_IGNORE_MESSAGE("Implement me!");
+	int x = getSignificantNum(100);
+	TEST_ASSERT_EQUAL(2,x);
+}
+
+void test_find_largest_number(void)
+{
+	int array[5] = {100,20,324,567,19};
+	int largestNum = getLargestNum(array);
+	int x = getSignificantNum(largestNum);
+	TEST_ASSERT_EQUAL(2,x);
+	TEST_ASSERT_EQUAL(567,largestNum);
 }
